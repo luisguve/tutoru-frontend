dab26def020b8e0fe2d7dfd0adbcde2c91f1c4c6
Luis G. Villegas <luisguve@gmail.com>
2021-07-14 21:55:19 -0300

    Introduje un hook que busca la solucion a los ejercicios comprados

--- pages/termodinamica/[slug].jsx
+++ pages/termodinamica/[slug].jsx
@@ -1,12 +1,67 @@
 import Head from "next/head"
+import { useContext, useState } from "react"
 
+import AuthContext from "../../context/AuthContext"
+import EjerciciosContext from "../../context/EjerciciosContext"
+import { API_URL } from "../../lib/urls"
 import SeccionEjercicios, { siteTitle } from '../../components/SeccionEjercicios'
 import BotonComprar from '../../components/BotonComprar'
 import { getEjerciciosIds, getEjercicio } from '../../lib/contenidos'
 import utilStyles from "../../styles/utils.module.css"
 
+/*
+* Este Hook verifica si el ejercicio esta en la lista de ejercicios que ha
+* comprado el usuario, y si está, pide la solucion a Strapi.
+*/
+const useSolucion = async (IDsEjercicios, id, token) => {
+  const [solucionDisponible, setSolucionDisponible] = useState(false)
+  const [loadingSolucion, setLoadingSolucion] = useState(false)
+  const [solucion, setSolucion] = useState(null)
+
+  useEffect(() => {
+    const fetchSolucion = async (id, token) => {
+      try {
+        setLoadingSolucion(true)
+        const solucionUrl = `${API_URL}/solucion/${id}`
+        const solucion_res = await fetch(solucionUrl, {
+          headers: {
+            "Authorization": `Bearer ${token}`
+          }
+        })
+        const data = await solucion_res.json()
+        setSolucion(data)
+      } catch (err) {}
+      setLoadingSolucion(false)
+    }
+
+    // Verifica si el usuario adquirió el ejercicio
+    if (IDsEjercicios && IDsEjercicios.length) {
+      if (IDsEjercicios.includes(id)) {
+        setSolucionDisponible(true)
+        // Cargar la solucion
+        fetchSolucion(id, token)
+      }
+    }
+  }, [IDsEjercicios, id])
+
+  return {
+    solucionDisponible,
+    solucion,
+    loadingSolucion
+  }
+}
+
 export default function Post({ ejercicio }) {
-  const { titulo, descripcion, categoria, categoriaFormato } = ejercicio
+  const { titulo, id, descripcion, categoria, categoriaFormato } = ejercicio
+  const { token } = useContext(AuthContext)
+  const { IDsEjercicios } = useContext(EjerciciosContext)
+
+  const {
+    solucionDisponible,
+    solucion,
+    loadingSolucion
+  } = useSolucion(IDsEjercicios, id, token)
+
   return (
     <SeccionEjercicios categoria={categoria} categoriaFormato={categoriaFormato}>
       <Head>
@@ -18,8 +73,18 @@ export default function Post({ ejercicio }) {
         <br />
         <div dangerouslySetInnerHTML={{ __html: descripcion}}></div>
         <br />
-        <bold>${ejercicio.precio}</bold>
-        <BotonComprar ejercicio={ejercicio} />
+        {
+          !solucionDisponible ?
+          <div>
+            <bold>${ejercicio.precio}</bold>
+            <BotonComprar ejercicio={ejercicio} />
+          </div>
+          :
+          loadingSolucion ?
+            <h3 style={{textAlign: "center"}}>Cargando solucion...</h3>
+          :
+            <div dangerouslySetInnerHTML={{ __html: solucion}}></div>
+        }
       </section>
     </SeccionEjercicios>
   )
