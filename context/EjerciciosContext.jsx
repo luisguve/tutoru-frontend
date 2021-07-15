bae7cb3d388e4c69a280c016aae60f667657a535
Luis G. Villegas <luisguve@gmail.com>
2021-07-14 21:55:56 -0300

    Movi el estado de los IDs de ejercicios a un nuevo contexto

---
+++ context/EjerciciosContext.jsx
@@ -0,0 +1,58 @@
+import { createContext, useContext, useState, useEffect } from "react"
+import { useToasts } from "react-toast-notifications"
+
+import AuthContext from "./AuthContext"
+import { API_URL } from "../lib/urls"
+
+const EjerciciosContext = createContext()
+
+export function EjerciciosProvider({children}) {
+  const [IDsEjercicios, setIDsEjercicios] = useState(null)
+  const { addToast } = useToasts()
+
+  const { token } = useContext(AuthContext)
+  useEffect(() => {
+    // Obtiene los IDs de los ejercicios que ha adquirido este usuario
+    const getEjercicios = async token => {
+      addToast("Obteniendo IDs de ejercicios comprados", { appearance: 'info' })
+      try {
+        const ejerciciosUrl = `${API_URL}/ejercicios/comprados-ids`
+
+        const ejercicios_res = await fetch(ejerciciosUrl, {
+          headers: {
+            "Authorization": `Bearer ${token}`
+          }
+        })
+
+        const ejercicios = await ejercicios_res.json()
+
+        if (!ejercicios || !ejercicios.length) {
+          addToast("Ningún ejercicio comprado", { appearance: 'info' })
+        } else {
+          let texto = `${ejercicios.length} ejercicios comprados`
+          if (ejercicios.length === 1) {
+            texto = "1 ejercicio comprado"
+          }
+          addToast(texto, { appearance: 'info' })
+        }
+
+        setIDsEjercicios(ejercicios)
+
+      } catch (err) {
+        console.log(err)
+        addToast("No se pudieron obtener los IDs de los ejercicios comprados", { appearance: 'error' })
+      }
+    }
+    if (token) {
+      console.log("Token:", token)
+      getEjercicios(token)
+    }
+  }, [token])
+  return (
+     <EjerciciosContext.Provider value={ IDsEjercicios }>
+       {children}
+     </EjerciciosContext.Provider>
+  )
+}
+
+export default EjerciciosContext
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,15 @@
 import '../styles/global.css'
 import { AuthProvider } from "../context/AuthContext"
+import { EjerciciosProvider } from "../context/EjerciciosContext"
 import { ToastProvider } from 'react-toast-notifications';
 
 export default function App({ Component, pageProps }) {
   return (
     <ToastProvider>
       <AuthProvider>
-        <Component {...pageProps} />
+        <EjerciciosProvider>
+          <Component {...pageProps} />
+        </EjerciciosProvider>
       </AuthProvider>
     </ToastProvider>
   )
