77cc4b6e31980ebddbaa74a064f13d304ebfd0a6
Luis G. Villegas <luisguve@gmail.com>
2021-07-19 13:37:27 -0300

    Guarda, actualiza y obtiene el contenido del carrito en el almacen local

--- context/CarritoContext.jsx
+++ context/CarritoContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 
 const CarritoContext = createContext()
 
@@ -7,19 +7,39 @@ export const CarritoProvider = props => {
   const [articulosIDs, setArticulosIDs] = useState([])
 
   const agregar = articulo => {
-    setArticulos([...articulos, articulo])
-    setArticulosIDs([...articulosIDs, {id: articulo.id}])
+    const nuevosArticulos = [...articulos, articulo]
+    setArticulos(nuevosArticulos)
+    const nuevosIDs = [...articulosIDs, {id: articulo.id}]
+    setArticulosIDs(nuevosIDs)
+    guardarSesion(nuevosArticulos, nuevosIDs)
   }
 
   const quitar = articulo => {
-    setArticulos(articulos.filter(a => a.id !== articulo.id))
-    setArticulosIDs(articulosIDs.filter(a => a.id !== articulo.id))
+    const nuevosArticulos = articulos.filter(a => a.id !== articulo.id)
+    setArticulos(nuevosArticulos)
+    const nuevosIDs = articulosIDs.filter(a => a.id !== articulo.id)
+    setArticulosIDs(nuevosIDs)
+    guardarSesion(nuevosArticulos, nuevosIDs)
   }
 
   const limpiar = () => {
     setArticulos([])
     setArticulosIDs([])
+    guardarSesion([], [])
   }
+
+  useEffect(() => {
+    const { data: carritoData } = obtenerSesion()
+    if (carritoData) {
+      const { articulos, articulosIDs } = carritoData
+      if (articulos) {
+        setArticulos(articulos)
+      }
+      if (articulosIDs) {
+        setArticulosIDs(articulosIDs)
+      }
+    }
+  }, [])
 
   return (
     <CarritoContext.Provider value={{articulos, articulosIDs , agregar, quitar, limpiar}}>
@@ -29,3 +49,34 @@ export const CarritoProvider = props => {
 }
 
 export default CarritoContext
+
+const obtenerSesion = () => {
+  if (typeof(Storage) !== undefined) {
+    const data = JSON.parse(localStorage.getItem("data"))
+    if (data) {
+      return {
+        data: data.carrito
+      }
+    }
+  }
+  return null
+}
+const guardarSesion = (articulos, articulosIDs) => {
+  if (typeof(Storage) !== undefined) {
+    const data = JSON.parse(localStorage.getItem("data"))
+    localStorage.setItem("data", JSON.stringify({
+      ...data,
+      carrito: {
+        articulos,
+        articulosIDs
+      }
+    }))
+  }
+}
+const limpiarSesion = () => {
+  if (typeof(Storage) !== undefined) {
+    const data = JSON.parse(localStorage.getItem("data"))
+    delete data.carrito
+    localStorage.setItem("data", JSON.stringify(data))
+  }
+}
