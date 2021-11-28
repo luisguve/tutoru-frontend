cbf65970091163fefaadae8334dae98bc5c81349
Luis G. Villegas <luisguve@gmail.com>
2021-11-28 18:50:37 -0400

    Eliminar el login de esta seccion

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,47 +4,17 @@ import Link from "next/link"
 import authContext from "../context/AuthContext"
 
 const Footer = () => {
-  const { user, loginUser: login } = useContext(authContext)
-
-  const [email, setEmail] = useState("")
-
-  const handleInput = e => {
-    setEmail(e.target.value)
-  }
-  const handleSubmit = e => {
-    e.preventDefault()
-    login(email)
-  }
+  const { user } = useContext(authContext)
 
   return (
-    <footer className="py-3">
-      {user ?
-        (<h6 className="user-info">
+    <footer className="py-4">
+      {
+        user &&
+        <h6 className="user-info">
           <Link href="/cuenta"><a>Iniciaste como {user.email}</a></Link>
-        </h6>)
-        :
-        (
-          <>
-          <h2>Inicia sesion:</h2>
-          <form onSubmit={handleSubmit}>
-            <label>
-              correo:
-              <input
-                type="email"
-                placeholder="tu dirección de correo"
-                value={email}
-                onChange={handleInput}
-                style={{marginLeft: 10}}
-              />
-            </label>
-            <button type="submit">
-              Ingresar
-            </button>
-          </form>
-          </>
-        )
+        </h6>
       }
-      <h6>Tutor universitario - copyright 2021</h6>
+      <h6 className="m-0">Tutor universitario - copyright 2021</h6>
     </footer>
   )
 }
