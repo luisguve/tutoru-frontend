9fcd7bc784f5a70bb64d93d796c54e1a6a105cd4
Luis G. Villegas <luisguve@gmail.com>
2021-12-08 19:27:07 -0400

    Configurando Google Analytics de la manera recomendada en https://mariestarck.com/add-google-analytics-to-your-next-js-application-in-5-easy-steps/

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react"
+import { useRouter } from "next/router"
 import { ToastProvider } from 'react-toast-notifications'
 
 import '../styles/global.scss'
@@ -13,8 +14,25 @@ import { CarritoProvider } from "../context/CarritoContext"
 import { LoginProvider } from "../context/LoginContext"
 import Carrito from "../components/Carrito"
 import LoginModal from "../components/LoginModal"
+import * as ga from '../lib/ga'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   useEffect(() => {
     typeof document !== undefined ? require('bootstrap/dist/js/bootstrap') : null
   }, [])
